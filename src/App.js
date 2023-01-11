19bd6e0b80251961ad60f3931444d5363b5201c6
Gefen hurvitz <gefenhurvitz@Gefens-MacBook-Air.local>
2023-01-11 20:31:06 +0200

    adding bootstrap and some styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import './App.css';
-import Footer from './components/Footer';
-import Header from './components/Header';
+import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+import { Container } from "react-bootstrap";
 
 function App() {
   return (
     <div>
-      <Header/>
-      <main>main</main>
-      <Footer/>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>WELCOME</h1>
+        </Container>
+      </main>
+      <Footer />
     </div>
   );
 }
