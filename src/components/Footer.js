4876b9e0ae7b6b5050a865a824f03dc3f2e55965
Gefen hurvitz <gefenhurvitz@Gefens-MacBook-Air.local>
2023-01-12 16:58:48 +0200

    working on the ProductScreen page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,19 @@
-import React from 'react'
-import { Container, Row, Col,  } from "react-bootstrap";
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 const Footer = () => {
   return (
     <footer>
-        <Container>
-            <Row>
-                <Col className='text-center py-3'>copyright &copy; GefenShop</Col>
-                </Row>
-        </Container>
+      <Container>
+        <Row>
+          <Col className="text-center py-3">copyright &copy; GefenShop</Col>
+        </Row>
+        <Row>
+          <Col className="text-center py-3">my Social Links</Col>
+        </Row>
+      </Container>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import { Row, Col, Image, Button, Card, ListGroup } from "react-bootstrap";
+import { Link, useParams } from "react-router-dom";
+import { Rating } from "../components/Rating";
+import products from "../products";
+
+
+// import ListGroup from 'react-bootstrap/ListGroup';
+
+
 
 const ProductScreen = () => {
-  return (
-    <div>product</div>
-  )
-}
+  let { id } = useParams();
+  const product = products.find((p) => p._id === id);
 
-export default ProductScreen
+  return (
+    <div>
+      <Link to={"/"} className="btn btn-danger my-3">
+        go back
+      </Link>
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} fluid />
+        </Col>
+        <Col md={3}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={3}></Col>
+      </Row>
+    </div>
+  );
+};
+
+export default ProductScreen;
